ad9fbde4ffe58cc5ec77a7c392bc36fa35b65e5d
CANSU TEKIN <ctekin22@gmail.com>
2023-07-25 03:17:35 +0000

    TABLE CREATED

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,11 @@
+CREATE TABLE song(  
+    title VARCHAR(100) NOT NULL,
+    artist VARCHAR(100) NOT NULL
+);
+
+INSERT INTO song VALUE('Let it be','Beatles'),            
+INSERT INTO song VALUE('Hotel California','Eagles'),             
+INSERT INTO song VALUE('Kashmir','Led Zeppelin'),
+
+
 
